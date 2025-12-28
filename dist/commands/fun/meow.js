053c61164f0ae3c414ce49166bbe09de4990ddd0
Mia <121320449+selenodot@users.noreply.github.com>
2025-12-28 16:23:11 -0700

    m

---
+++ dist/commands/fun/meow.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const nhandler_1 = require("nhandler");
+const index_1 = require("../../index");
+const util_1 = require("../../util");
+class default_1 extends util_1.BaseCommand {
+    name = "meow";
+    description = "Meow.";
+    metadata = {
+        category: "Messages",
+    };
+    integrationTypes = [0, 1];
+    contexts = [0, 1, 2];
+    async run(interaction) {
+
+        try {
+            await interaction.reply({ content: "meow" });
+        }
+        catch (err) {
+            throw new nhandler_1.ExecutionError(`Failed to send text.\n${err.message}`);
+        }
+    }
+}
+exports.default = default_1;
+//# sourceMappingURL=say.js.map
